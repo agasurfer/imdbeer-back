37ef1b78f0a1ef16e42b7afa2b1caad23a3bf43d
agasurfer <agasurfer@hotmail.com>
2025-01-22 13:52:07 +0100

    port change

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.delete("/styles/:id/comments", async (req, res) => {
 
 //Listening
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
     console.log(`Listening on port ${PORT}`);
     
